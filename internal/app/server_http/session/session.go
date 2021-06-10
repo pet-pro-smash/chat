e6d6ee1ed01eabcb6b7129cee5b72b468e93ba82
user <kiberlom@yandex.ru>
2021-06-10 11:31:40 +0400

    сессии начало

--- internal/app/server_http/session/session.go
+++ internal/app/server_http/session/session.go
@@ -1,1 +1,34 @@
 package session
+
+import "time"
+
+// данные с сессиями
+type sessionStore struct {
+	store map[string]sesUser
+}
+
+// данные сессии одного клиента
+type sesUser struct {
+	dateCreate    time.Time // дата создания сессии
+	dateLastVisit time.Time // время последнего использования
+	sessionID     string    // индентификатор сессии UUID
+	userID        string    // id user
+
+}
+
+// инициализируем сессии
+func NewSeesion() *sessionStore {
+	return &sessionStore{
+		store: make(map[string]sesUser),
+	}
+}
+
+// проверка наличия сессии по индификатору UUID
+func (s *sessionStore) Check(string) (bool, error) {
+	return true, nil
+}
+
+// создание новой сессии
+func (s *sessionStore) Create() (string, error) {
+	return "UUID", nil
+}
